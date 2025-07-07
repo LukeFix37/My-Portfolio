304930fdfb76ebd5df83f409c467593121d8a2da
LukeFix37 <159561664+LukeFix37@users.noreply.github.com>
2025-07-07 16:52:40 -0400

    updated routes

--- my-portfolio/src/app/app.routes.ts
+++ my-portfolio/src/app/app.routes.ts
@@ -1,14 +1,3 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes } from '@angular/router';
 
-const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  // Add your other routes here
-  { path: '**', redirectTo: '/home' } // Catch-all route
-];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes, { useHash: false })],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+export const routes: Routes = [];
